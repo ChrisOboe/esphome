2c40ec1fe343390eb16f52d3b50ae3d7dc6a385f
Chris Oboe <chris@oboe.email>
2022-11-06 17:37:10 +0100

    add support for esp32; add inkplate weatherstation

--- flake.nix
+++ flake.nix
@@ -1,20 +1,22 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/f01cf053f6a800c7ce80f79a0afb9e87f7590142";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            pkgs.esphome
-            pkgs.gnumake
-          ];
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          pkgs.esphome
+          pkgs.gnumake
+        ];
+      };
+    });
 }
-
