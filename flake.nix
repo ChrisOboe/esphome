d480be6149aeb817819f918b86cd84515868494b
Chris Oboe <chris@oboe.email>
2023-08-06 19:44:40 +0200

    update stuff

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/pull/229461/head.tar.gz";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
