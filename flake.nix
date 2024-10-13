0fef865cc4a19c5bf70cb6970ff275c1a700a23c
Chris Oboe <chris@oboe.email>
2024-10-13 12:23:13 +0200

    update stuff

--- flake.nix
+++ flake.nix
@@ -1,25 +1,35 @@
 {
   inputs = {
-    #nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/pull/229461/head.tar.gz";
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          esphome
-          yq-go
-          git-crypt
-        ];
-      };
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (esphome.overrideAttrs {
+              src = fetchFromGitHub {
+                owner = "esphome";
+                repo = "esphome";
+                rev = "refs/tags/2024.8.3";
+                hash = "sha256-AeiEP1D2u2u8LMNKdBvoA2M3uO6Z9oSG2i5HwGin80M=";
+              };
+            })
+            yq-go
+            git-crypt
+          ];
+        };
+      }
+    );
 }
