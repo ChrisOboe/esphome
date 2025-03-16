cdcf7774cdf739a1769268f45803a13f93790871
Chris Oboe <chris@oboe.email>
2025-03-16 16:13:37 +0100

    update deps

--- flake.nix
+++ flake.nix
@@ -18,16 +18,16 @@
       {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
-            #esphome
+            esphome
             # needs at least 2025 version since otherwise mqtt wont be unique
-            (esphome.overrideAttrs {
-              src = fetchFromGitHub {
-                owner = "esphome";
-                repo = "esphome";
-                rev = "refs/tags/2025.2.0b3";
-                hash = "sha256-s8R/0QK6tuOzy8wllrHVU5NkkoNH4wtx04dPWIrUXNs=";
-              };
-            })
+            #(esphome.overrideAttrs {
+            #  src = fetchFromGitHub {
+            #    owner = "esphome";
+            #    repo = "esphome";
+            #    rev = "refs/tags/2025.2.0b3";
+            #    hash = "sha256-s8R/0QK6tuOzy8wllrHVU5NkkoNH4wtx04dPWIrUXNs=";
+            #  };
+            #})
             yq-go
             git-crypt
           ];
